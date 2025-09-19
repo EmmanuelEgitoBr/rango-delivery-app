dc136f84065be50fae90c3feae85053632175ceb
Emmanuel Egito Oliveira Candeira Marinho <e_egito@yahoo.com>
2025-09-19 08:37:44 -0300

    Criação do backend

---
+++ backend/src/Food.Delivery.Store/Food.Delivery.Store.Shared.Messages/Messages/OrderPlaced.cs
@@ -0,0 +1,9 @@
+﻿namespace Food.Delivery.Store.Shared.Messages.Messages;
+
+public sealed record OrderPlaced
+{
+    public required Guid OrderId { get; init; }
+    public required Guid CustomerId { get; init; }
+    public required DateTime OrderDate { get; init; }
+    public required List<OrderItem> Items { get; init; }
+}
